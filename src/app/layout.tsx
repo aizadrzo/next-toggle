d9a531a9e70e73c3783551e3cf57df8fd94c7b32
Aizad <aizadridzoo@gmail.com>
2025-10-21 21:06:12 +0800

    chore: udpate marketing folder component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { SiteHeader } from "@/components/site-header";
-import { SiteFooter } from "@/components/site-footer";
+import { SiteHeader, SiteFooter } from "@/components/marketing";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
