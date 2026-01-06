05ab1b89a21db3e07045f0cfc3dbc25ee13893d4
aizadrzo <aizadridzoo@gmail.com>
2026-01-07 00:05:13 +0800

    feat: add new integrations section, logo component, and associated image assets, updating feature3 to display integrations.

--- src/components/feature3.tsx
+++ src/components/feature3.tsx
@@ -1,5 +1,5 @@
 import { cn } from "@/lib/utils";
-import { AnimatedBeamDemo } from "./marketing-tools";
+import Image from "next/image";
 
 export function Feature3({ className }: { className?: string }) {
   return (
@@ -10,8 +10,8 @@ export function Feature3({ className }: { className?: string }) {
             <div className="mb-6 md:mb-8 lg:mb-0">
               <img
                 alt="placeholder hero"
-                className="aspect-4/3 w-full rounded-md border border-border object-cover"
-                src="https://deifkwefumgah.cloudfront.net/shadcnblocks/block/placeholder-1.svg"
+                className="aspect-4/3 w-full rounded-md border border-border bg-zinc-50 object-cover"
+                src="/images/decorative/decorative-4.png"
               />
             </div>
           </div>
@@ -31,9 +31,86 @@ export function Feature3({ className }: { className?: string }) {
         <div className="flex-row-reverse lg:flex lg:gap-x-4">
           <div className="lg:w-1/2">
             <div className="mb-6 md:mb-8 lg:mb-0">
-              <div className="aspect-4/3 w-full rounded-md border border-border h-full">
-                <div className="flex items-center justify-center">
-                  <AnimatedBeamDemo />
+              <div className="aspect-4/3 w-full rounded-md border border-border h-full grid place-content-center">
+                <div className="dark:bg-muted/50 relative mx-auto w-fit h-full">
+                  <div
+                    aria-hidden
+                    className="bg-radial to-background dark:to-background absolute inset-0 z-2 from-transparent to-75%"
+                  />
+                  <div className="mx-auto mb-2 flex w-fit justify-center gap-2">
+                    <IntegrationCard>
+                      <Image
+                        alt="shopify"
+                        src="/images/integration-logos/shopify-logo.svg"
+                        className="w-10 sm:w-15"
+                        width={100}
+                        height={100}
+                      />
+                    </IntegrationCard>
+                    <IntegrationCard>
+                      <Image
+                        alt="klaviyo"
+                        src="/images/integration-logos/klaviyo-logo.svg"
+                        className="w-10 sm:w-15"
+                        width={100}
+                        height={100}
+                      />
+                    </IntegrationCard>
+                  </div>
+                  <div className="mx-auto my-2 flex w-fit justify-center gap-2">
+                    <IntegrationCard>
+                      <Image
+                        alt="meta"
+                        src="/images/integration-logos/meta-logo.svg"
+                        className="w-10 sm:w-15"
+                        width={100}
+                        height={100}
+                      />
+                    </IntegrationCard>
+                    <IntegrationCard
+                      borderClassName="shadow-black-950/10 shadow-xl border-black/25 dark:border-white/25"
+                      className="dark:bg-white/10"
+                    >
+                      <Image
+                        alt="meta"
+                        src="/images/brand/logo-light.svg"
+                        className="w-10 sm:w-15"
+                        width={100}
+                        height={100}
+                      />
+                    </IntegrationCard>
+                    <IntegrationCard>
+                      <Image
+                        alt="tiktok"
+                        src="/images/integration-logos/tiktok-logo.svg"
+                        className="w-10 sm:w-15"
+                        width={100}
+                        height={100}
+                      />
+                    </IntegrationCard>
+                  </div>
+
+                  <div className="mx-auto flex w-fit justify-center gap-2">
+                    <IntegrationCard>
+                      <Image
+                        alt="google"
+                        src="/images/integration-logos/google-logo.svg"
+                        className="w-10 sm:w-15"
+                        width={100}
+                        height={100}
+                      />
+                    </IntegrationCard>
+
+                    <IntegrationCard>
+                      <Image
+                        alt="semrush"
+                        src="/images/integration-logos/semrush-logo.svg"
+                        className="w-15 sm:w-20"
+                        width={100}
+                        height={100}
+                      />
+                    </IntegrationCard>
+                  </div>
                 </div>
               </div>
             </div>
@@ -57,7 +134,7 @@ export function Feature3({ className }: { className?: string }) {
               <img
                 alt="placeholder hero"
                 className="aspect-4/3 w-full rounded-md border border-border object-cover"
-                src="https://deifkwefumgah.cloudfront.net/shadcnblocks/block/placeholder-1.svg"
+                src="/images/decorative/decorative-5.png"
               />
             </div>
           </div>
@@ -78,3 +155,31 @@ export function Feature3({ className }: { className?: string }) {
     </div>
   );
 }
+
+const IntegrationCard = ({
+  children,
+  className,
+  borderClassName,
+}: {
+  children: React.ReactNode;
+  className?: string;
+  borderClassName?: string;
+}) => {
+  return (
+    <div
+      className={cn(
+        "bg-background relative flex size-20 sm:size-30 rounded-xl dark:bg-transparent",
+        className
+      )}
+    >
+      <div
+        role="presentation"
+        className={cn(
+          "absolute inset-0 rounded-xl border border-black/20 dark:border-white/25",
+          borderClassName
+        )}
+      />
+      <div className="relative z-20 m-auto size-fit">{children}</div>
+    </div>
+  );
+};
